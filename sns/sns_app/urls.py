bba6527321813db0600751543d4ef23823f66d4c
py-py-py <37635137+py-py-py@users.noreply.github.com>
2020-01-06 12:33:00 +0900

    update urls.py

--- sns/sns_app/urls.py
+++ sns/sns_app/urls.py
@@ -7,8 +7,6 @@ app_name = "sns_app"
 urlpatterns = [
     path('index/', views.thread_index_view, name="index"),
     path("<int:thread_id>/", views.thread_view, name="thread"),
-    # path("<int:pk>/create_post/", views.CreateResPost.as_view(), name="create_post"),
-    path("<int:thread_id>/create_post/", views.create_respost_ver2, name="create_post"),
-    path("<int:thread_id>/post/<int:respost_id>/", views.res_detail_view, name="detail"),
+    path("<int:pk>/create_post/", views.CreateResPost.as_view(), name="create_post"),
     path("create_thread/", views.CreateThreadView.as_view(), name="create_thread"),
 ]
